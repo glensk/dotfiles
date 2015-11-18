ddca6e4e8c85f7bb6599db38b2c7e09d7ebb6289
glensk <albert.glensk@gmail.com>
2015-11-18 17:23:44 +0100

    Wed Nov 18 17:23:44 CET 2015

--- bin/gitdown.sh
+++ bin/gitdown.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 echo "#### now in `pwd` #####################";echo
 echo '#### git remote -v update #############';echo;git remote -v update;echo
-echo '#### git status -u ####################';echo;git status -u;echo
-echo '#### git commit -a -m "`date`"#########';echo;git commit -a -m "`date`";echo
-echo '#### git pull #########################';echo;git pull
+echo ""
+echo ""
+echo '#### git status -u ####################'
+git status -u
+echo "--"
+echo '#### git commit -a -m "`date`"#########'
+echo "--"
+git commit -a -m "`date`"
+echo "--"
+echo '#### git pull ######################### this makes merge'
+echo "--"
+git pull
 
 
 
