5a0fb521151c1f123bf54d7c956e0b10ea0145b2
glensk <albert.glensk@gmail.com>
2015-11-18 17:43:10 +0100

    Wed Nov 18 17:43:10 CET 2015

--- bin/gitdown.sh
+++ bin/gitdown.sh
@@ -5,12 +5,23 @@ echo ""
 echo ""
 echo '#### git status -u ####################'
 git status -u
+echo 
+echo 
+echo 
+echo 
+echo 
 echo "--"
 echo '#### git commit -a -m "`date`"#########'
 echo "--"
+echo 
+echo 
+echo 
 git commit -a -m "`date`"
 echo "--"
+echo "--"
 echo '#### git pull ######################### this makes merge'
+echo "--"
+echo "--"
 echo "--"
 git pull
 
