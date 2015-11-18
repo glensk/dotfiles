eb301fd3c236e83bd144d17c363a351ece73b783
glensk <albert.glensk@gmail.com>
2015-11-18 18:13:25 +0100

    Wed Nov 18 18:13:24 CET 2015

--- generalrc/generalrc_prompt_zsh.sh
+++ generalrc/generalrc_prompt_zsh.sh
@@ -4,7 +4,8 @@
 #echo yo $myprompttime
 #PROMPT="%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg_no_bold[yellow]%}%1~ %{$reset_color%}%#"
 #RPROMPT="[%{$fg_no_bold[yellow]%}%?%{$reset_color%}]"
-local prompttime="%{$fg[$myprompttime]%}%*%{$reset_color%}"
+#local prompttime="%{$fg[$myprompttime]%}%*%{$reset_color%}"
+local prompttime="%{$fg[magenta]%}%*%{$reset_color%}"
 local user_host="%{$fg[$myprompthostuser]%}%n@%m%{$reset_color%}"
 local current_dir="%{$fg[$mypromptpath]%}%d%{$reset_color%}"
 PROMPT="${prompttime} ${user_host} ${current_dir} ${svn_branch}
