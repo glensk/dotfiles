7b10ef335e54ccda5f64f625417089202c87c275
glensk <albert.glensk@gmail.com>
2015-11-19 12:29:46 +0100

    Thu Nov 19 12:29:45 CET 2015

--- generalrc/generalrc_hostdependent.sh
+++ generalrc/generalrc_hostdependent.sh
@@ -55,7 +55,7 @@ else
     myprompttime="setenv myprompttime turquoise"
     myprompthostuser="setenv myprompthostuser turquoise"
     mypromptpath="setenv mypromptpath turquoise"
-    module="module load git intel impi mkl grace mathematica/9.0.1 vasp/5.3.5" 
+    module="setenv ka kb" #module load git intel impi mkl grace mathematica/9.0.1 vasp/5.3.5" 
     add="limit stacksize unlimited;setenv vaspq /u/aglen/vasp/vasp_4.6_lj_morse_alles_copied_FFTWPLANS_works/vasp.v1"
 else
     if [ "`echo $host | grep -o cmmd`" = "cmmd" ];then
