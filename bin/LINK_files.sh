4042c1bf11140cbaaed5b7acbf6e382f60bc7af9
glensk <albert.glensk@gmail.com>
2015-11-19 12:25:48 +0100

    Thu Nov 19 12:25:47 CET 2015

--- bin/LINK_files.sh
+++ bin/LINK_files.sh
@@ -411,3 +411,5 @@ if [ -e "$HOMEPATH/.config/terminator" ]; then
     [ ! -e "$file" ] && echo $file link && ln -s $dotfiles/terminator/config $file
     fi
 chmod 600 ~/.ssh/config
+cd $dotfiles
+cd autojump && ./install.py && cd $dotfiles
