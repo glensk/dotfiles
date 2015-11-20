e7bb6567dedd1053a3a86bcb90bdd28b7c3a00ab
glensk <albert.glensk@gmail.com>
2015-11-20 16:03:17 +0100

    Fri Nov 20 16:03:17 CET 2015

--- generalrc/generalrc_alias_renew.sh
+++ generalrc/generalrc_alias_renew.sh
@@ -9,11 +9,18 @@ mkalias () {
     eval `aliastcshtobash $1 "$2"`
 }
 
-echo $currenthost
-echo $whichalias
-echo $currentshell
-aliasfile=$generalrc/generalrc_alias_$currenthost
-source $generalrc/generalrc_alias_.sh $whichalias $currentshell 
+#echo $currenthost
+#echo $whichalias
+#echo $currentshell
+
+if [ "$currenthost" = "" ];then
+[ "`echo $host | grep -o cmpc`" = "cmpc" ] && currenthost=cmpc
+[ "`echo $host | grep -o cmmd`" = "cmmd" ] && currenthost=cmmd
+[ "`echo $host | grep -o cmmc`" = "cmmc" ] && currenthost=cmmc
+fi
+aliasfile=$HOME/Dropbox/scripts/dotfiles/generalrc/generalrc_alias_$currenthost
+#source $HOME/Dropbox/scripts/dotfiles/generalrc_alias_.sh $whichalias $currentshell 
+source $HOME/Dropbox/scripts/dotfiles/generalrc_alias_.sh mkalias $currentshell 
 rm -f $aliasfile
 #alias | grep -v "^-" | grep -v "^../=" | sed 's|\(.*\)|alias \1|' > $aliasfile
 alias | grep -v "^-" | grep -v "^../=" > $aliasfile
