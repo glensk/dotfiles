a534f047a361814cd5d2fd1aa77799ba8abd1fc3
glensk <albert.glensk@gmail.com>
2016-01-22 11:01:34 +0100

    Fri Jan 22 11:01:33 CET 2016

---
+++ sources/atomsk_install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# from http://atomsk.univ-lille1.fr/dl.php
+# to compile : go to src and type make atomsk (this was successful on mac with gfortran)
+
+# I expect to be in sources folder
+sources=$HOME/Dropbox/scripts/dotfiles/sources
+[ "`pwd`" != "$sources" ] && echo "not in sources folder" && exit
+echo "OK, I am in sources folder :)"
+file=atomsk_b0.8.3
+tar -xvf $file.tar.gz
+cd $file/src
+make atomsk
+cp atomsk $sources/bin/
+cd $sources
+#rm -rf $file
