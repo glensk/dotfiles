928542d1a5cd0c535ce0ba53efb2510bac233980
glensk <albert.glensk@gmail.com>
2016-11-03 09:36:55 +0100

    Thu Nov  3 09:36:54 CET 2016

--- bin/wcquick.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-[ ! -e "$1" ] && echo 'please specify the file by as first argument $1' && exit -1
-file=$1; string=$(wc -c $file); bite=${string% *}; okay=$(echo "scale=2; $bite/1024" | bc);friend=$(echo -e "$file $okay" "kb"); echo -e "$friend"
