85272181ec3482177c370a7407402333efaf70c3
glensk <albert.glensk@gmail.com>
2016-11-22 09:57:58 +0100

    Tue Nov 22 09:57:58 CET 2016

--- generalrc/generalrc_hostdependent.sh
+++ generalrc/generalrc_hostdependent.sh
@@ -55,7 +55,7 @@ else
     myprompttime="setenv myprompttime cyan"
     myprompthostuser="setenv myprompthostuser cyan"
     mypromptpath="setenv mypromptpath cyan"
-    module="module load git intel impi mkl grace mathematica/10.3.1 vasp/5.3.5" 
+    module="module load git intel impi mkl grace mathematica/10.3.1 vasp/5.3.5 anaconda" 
     add="limit stacksize unlimited;setenv vaspq /u/aglen/vasp/vasp_4.6_lj_morse_alles_copied_FFTWPLANS_works/vasp.v1;setenv submithost cmmc002"
 #else  # cmmd does not exist anymore
 #    if [ "`echo $host | grep -o cmmd`" = "cmmd" ];then
