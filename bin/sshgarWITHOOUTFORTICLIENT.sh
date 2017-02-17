a18eb2184abbf57ebeb9231c52fc2f378b62e6e1
glensk <albert.glensk@gmail.com>
2017-02-17 16:52:34 +0100

    Fri Feb 17 16:52:34 CET 2017

--- bin/sshgarWITHOOUTFORTICLIENT.sh
+++ bin/sshgarWITHOOUTFORTICLIENT.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+to="cmmc001"
+[ "$1" != "" ] && to="cmmc002"
+echo to:$to
 
 a=64 b=244 c=208
 echo "\033]6;1;bg;red;brightness;$a\a"
 echo "\033]6;1;bg;green;brightness;$b\a"
 echo "\033]6;1;bg;blue;brightness;$c\a"
 
-ssh -Y -X -t aglen@gate.rzg.mpg.de ssh -X -Y  aglen@cmmc001.bc.rzg.mpg.de
+ssh -Y -X -t aglen@gate.rzg.mpg.de ssh -X -Y  aglen@$to.bc.rzg.mpg.de
