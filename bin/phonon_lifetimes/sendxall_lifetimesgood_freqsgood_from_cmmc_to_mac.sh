94260f004c857ad94636944ddbe2bec5330654f9
glensk <albert.glensk@gmail.com>
2017-04-20 12:22:13 +0200

    Thu Apr 20 12:22:13 CEST 2017

--- bin/phonon_lifetimes/sendxall_lifetimesgood_freqsgood_from_cmmc_to_mac.sh
+++ bin/phonon_lifetimes/sendxall_lifetimesgood_freqsgood_from_cmmc_to_mac.sh
@@ -14,6 +14,6 @@ if [ "$host" = "mac" ];then
     echo "in",`hostname`
     if [ -e "/Users/glensk/Dropbox/proj/proj_current/__2017.01_phonon_linewidth_al/__2017.01_phonon_lifetimes_4_ab2017/check_20_python_plotting" ];then
         cd /Users/glensk/Dropbox/proj/proj_current/__2017.01_phonon_linewidth_al/__2017.01_phonon_lifetimes_4_ab2017/check_20_python_plotting
-        rx
+        recievex
     fi
 fi
