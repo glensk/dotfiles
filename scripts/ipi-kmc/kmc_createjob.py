51e816abdc998ff3535ae5fbf4ce8d9784189768
glensk <albert.glensk@gmail.com>
2018-10-17 16:57:04 +0200

    Wed Oct 17 16:57:04 CEST 2018

--- scripts/ipi-kmc/kmc_createjob.py
+++ scripts/ipi-kmc/kmc_createjob.py
@@ -22,7 +22,8 @@ nvac=1
 a0=4.057
 
 nseeds=4
-nseeds=1 # for testing
+nseeds=2 # for testing
+seedplus=32346
 steps=200000
 steps=200 # for testing
 
@@ -56,13 +57,15 @@ def get_positions_files(scripts,ncell,nmg,nsi,nvac,a0):
     number_of_atoms = sc**3 - nva
     nal = number_of_atoms - nmg - nsi
     path=scripts+'/kmc_positions/'
-    filename = "al"+str(sc)+"x"+str(sc)+"x"+str(sc)+"_alat"+str(alat)+"_"+str(nal)+"al_"+str(nsi)+"si_"+str(nmg)+"mg_"+str(nva)+"va_"+str(number_of_atoms)+"atoms"+".xyz"
+    filename = "al"+str(sc)+"x"+str(sc)+"x"+str(sc)+"_alat"+str(alat)+"_"+str(nal)+"al_"+str(nsi)+"si_"+str(nmg)+"mg_"+str(nva)+"va_"+str(number_of_atoms)+"atoms"
     print('filename',filename)
-    filepath = path+filename
-    if os.path.exists(filepath):
-        return filepath,filename
+    xyz = path+filename+'.xyz'
+    ipi = path+filename+'.ipi'
+    lmp = path+filename+'.xyz.lmp'
+    if os.path.exists(xyz):
+        return xyz,ipi,lmp,filename
     else:
-        sys.exit('filepath '+filepath+" not found!")
+        sys.exit('xyz files '+xyz+" not found!")
 
 
 
@@ -70,18 +73,22 @@ def get_positions_files(scripts,ncell,nmg,nsi,nvac,a0):
 #def makejobs(ncell,nvac,nmg,nsi):
 directory = str(ncell)+"x"+str(ncell)+"x"+str(ncell)+"_"+str(nvac)+"Vac_"+str(nmg)+"Mg_"+str(nsi)+"Si"
 mkdir(directory)
-seeds=np.arange(nseeds)+32345
-xyzpath,xyzname = get_positions_files(scripts,ncell,nmg,nsi,nvac,a0)
-print('xyzpath',xyzpath)
+seeds=np.arange(nseeds)+seedplus
+xyz,ipi,lmp,positionsname = get_positions_files(scripts,ncell,nmg,nsi,nvac,a0)
+print('xyz',xyz)
+print('ipi',ipi)
+print('lmp',lmp)
+print('positionsname',positionsname)
 for seed in seeds:
-    #print('seed:',seed)
     jobdir = directory+'/seed'+str(seed)
     print('jobdir',jobdir)
     if os.path.exists(jobdir):
         sys.exit("jobdirectory "+str(jobdir)+" already exists!")
     mkdir(jobdir)
     copyfile(scripts+"input-runner.xml", jobdir+"/input-runner.xml")
-    copyfile(xyzpath, jobdir+"/"+xyzname)
+    #copyfile(xyz, jobdir+"/"+positionsname+".xyz")
+    copyfile(ipi, jobdir+"/"+positionsname+".ipi")
+    copyfile(lmp, jobdir+"/data.lmp")
     copyfile(scripts+"in.lmp", jobdir+"/in.lmp")
     copyfile(scripts+"submit.sh", jobdir+"/submit.sh")
     sed(jobdir+"/input-runner.xml",'<total_steps>.*</total_steps>','<total_steps> '+str(steps)+' </total_steps>')
@@ -91,7 +98,7 @@ for seed in seeds:
     sed(jobdir+"/input-runner.xml",'<nsi>.*</nsi>','<nsi> '+str(nsi)+' </nsi>')
     sed(jobdir+"/input-runner.xml",'<nmg>.*</nmg>','<nmg> '+str(nmg)+' </nmg>')
     sed(jobdir+"/input-runner.xml",'<nvac>.*</nvac>','<nvac> '+str(nvac)+' </nvac>')
-    sed(jobdir+"/input-runner.xml",'<file mode="xyz" units="angstrom">.*</file>','<file mode="xyz" units="angstrom"> '+str(xyzname)+' </file>')
+    sed(jobdir+"/input-runner.xml",'<file mode="xyz" units="angstrom">.*</file>','<file mode="xyz" units="angstrom"> '+str(positionsname)+'.ipi </file>')
     sed(jobdir+"/input-runner.xml",'<neval>.*</neval>','<neval> '+str(neval)+' </neval>')
 
     sed(jobdir+"/submit.sh",'#SBATCH --nodes=.*','#SBATCH --nodes='+str(nodes))
@@ -99,5 +106,7 @@ for seed in seeds:
     sed(jobdir+"/submit.sh",'--exclusive -n .* --mem','--exclusive -n '+str(lmp_par)+' --mem')
     sed(jobdir+"/submit.sh",'for i in `seq.*','for i in `seq '+str(ipi_inst)+'`')
 
+    cwd = os.getcwd()
     os.chdir(jobdir)
-    call(["sbatch","submit.sh"]) #,shell=True)
+    call(["sbatch","submit.sh"])
+    os.chdir(cwd)
