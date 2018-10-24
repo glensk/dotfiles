1fa81028f31fd2c43c1ea7610d838894f958c116
glensk <albert.glensk@gmail.com>
2018-10-24 11:19:23 +0200

    Wed Oct 24 11:19:23 CEST 2018

---
+++ aliases/tar.bzip2_file_or_folder.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+[ "$1" = "" ] && echo 'need $1 to the the foldername' && exit
+[ -e "$1.tar.bzip2" ] && echo $1.tar.bzip2 does already exist && exit
+tar --use-compress-program=lbzip2 -cvf "$1.tar.bzip2" $1
+
