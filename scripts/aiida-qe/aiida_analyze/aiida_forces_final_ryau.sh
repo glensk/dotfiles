7500e13a5e7b8298671dfd0c6bd9a75de8163ac8
glensk <albert.glensk@gmail.com>
2018-10-24 18:00:24 +0200

    Wed Oct 24 18:00:24 CEST 2018

---
+++ scripts/aiida-qe/aiida_analyze/aiida_forces_final_ryau.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+atoms=`head -100 aiida.out | grep "number of atoms/cell" | awk '{print $5}'`
+line=`grep -n "^     Forces acting on atoms (cartesian axes, Ry/au):" aiida.out | tail -1 | sed 's|:.*||' | awk '{print $1+2}'`
+lineend=`echo $line $atoms | awk '{print $1+$2-1}'`
+#echo atoms: $atoms, $line $lineend
+#sed -n ''"$line"','"$lineend"'p' aiida.out | awk '{print $7,$8,$9}'
+#sed -n ''"$line"','"$lineend"'p' aiida.out | awk '{print $7*25.711032,$8*25.711032,$9*25.711032}'  # in eV
+#sed -n ''"$line"','"$lineend"'p' aiida.out | awk '{print sqrt(($7*25.711032)^2),sqrt(($8*25.711032)^2),sqrt(($9*25.711032)^2)}' 
+sed -n ''"$line"','"$lineend"'p' aiida.out | awk '{print sqrt(($7)^2),sqrt(($8)^2),sqrt(($9)^2)}' 
