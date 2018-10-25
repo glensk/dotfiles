c25fe527371ecbda68b662c0e363fce3e3a9b200
glensk <albert.glensk@gmail.com>
2018-10-25 16:29:14 +0200

    Thu Oct 25 16:29:14 CEST 2018

--- generalrc/generalrc_.sh
+++ generalrc/generalrc_.sh
@@ -5,6 +5,7 @@
 # shell, all set variables will be loaded by either shell (bash or tcsh or zsh) 
 # since the setenv function is defined for the bash shell in the first loaded 
 # $generalrc/generalrc_necessary_bash.sh script.
+###################################################################################
 
 ###################################################################################
 # this has to be the first line  (loads for zsh and bash); defines: tab-color; module; mkalias
@@ -15,23 +16,23 @@
 # HOST dependent variables (prompt, module load, anaconda (on cmmc with module load) ...)
 ###################################################################################
 eval `$generalrc/generalrc_hostdependent.sh`   
-# defines:  {onmac, oncmmc, oncmdft} to true or false
-#           currenthost to {onmac, oncmmc, oncmdft}
-#           myshell to {zsh,bash, tcsh}
-#           prompt, promptcolor
-#echo ONMAC $onmac
-#echo WHICHALIAS $whichalias  
-#echo CURRENTHOST $currenthost
+            # defines:  
+            # --------
+            # onmac, oncmmc, oncmdft} to true or false
+            # currenthost to {onmac, oncmmc, oncmdft}
+            # myshell to {zsh,bash, tcsh}
+            # prompt, promptcolor
+            # printloadstats
+            # echo ONMAC $onmac
+            # echo WHICHALIAS $whichalias  
+            # echo CURRENTHOST $currenthost
 
-# printloadstat was defined in $generalrc/generalrc_hostdependent.sh
 
 ###################################################################################
 # PATH, PYTHONPATH  (anaconda on mac)
 # PYTHONPATH should not be set (according to anaconda/miniconda)
 ###################################################################################
 eval `$generalrc/generalrc_path.sh $currenthost` 
-#[ "$oncmmc" = "true" ] && load_local_anaconda DOES NOT WORK
-#[ "`hostname`" = "cmmc002" ] && load_local_anaconda
 
 #export C_INCLUDE_PATH="/Users/glensk/.miniconda2/include"
 #@# time without aliases          : 0.38
@@ -41,14 +42,9 @@ eval `$generalrc/generalrc_path.sh $currenthost`
 ##############################################
 # ALIASES
 ##############################################
-# how to create a new alias (which works on every host)
-# 1. shell: virca  --> add the alias you need
-# 2. to renew aliasfile: ra   (this only creates a new alias file)
-# 3. START A NEW SHELL (also you typed ra!) 
 [ ! -e "$generalrc/generalrc_alias_$currenthost" ] && $generalrc/generalrc_alias_renew.sh
 
-# general aliases (work) everywhere
-#echo "currenthost :$currenthost:"
+# aliases which worc everywhere
 [ "$currentshell" != "tcsh" ] && source $generalrc/generalrc_alias_$currenthost
 [ "$currentshell" = "tcsh" ] && source $generalrc/generalrc_alias_.sh $whichalias $currentshell
 
@@ -60,11 +56,6 @@ eval `$generalrc/generalrc_path.sh $currenthost`
 ##############################################
 # PROMPT & tabcolor
 ##############################################
-#echo 
-#echo
-#echo "currentshell $currentshell"
-#echo 
-#echo
 [ "$currentshell" = "tcsh" ] && source $generalrc/generalrc_prompt_tcsh.sh
 [ "$currentshell" = "zsh" ] && source $generalrc/generalrc_prompt_zsh.sh
 [ "$currentshell" = "bash" ] && source $generalrc/generalrc_prompt_bash.sh
@@ -80,8 +71,8 @@ eval `$generalrc/generalrc_path.sh $currenthost`
 #echo MYPROMPTTIME $myprompttime;
 #echo 
 
-[ "`hostname`" = "cmmc002" ] && load_local_anaconda
-[ "`hostname`" = "cmmc001" ] && load_local_anaconda
+#[ "`hostname`" = "cmmc002" ] && load_local_anaconda
+#[ "`hostname`" = "cmmc001" ] && load_local_anaconda
 ##############################################
 # set host unspecific variables  
 ##############################################
@@ -101,7 +92,7 @@ setenv LANG C     # necessary for perl
     
 setenv GIT_EDITOR vim
 setenv PAGER most
-#setenv BROWSER chrome
+setenv BROWSER chrome
 setenv BROWSER open # is necessary for opening jupyter notebook files
 setenv LC_ALL en_US.UTF-8
 setenv GREP_COLOR 31    # red; some greps have colorized ouput. enable...
@@ -110,7 +101,6 @@ setenv GREPCOLOR 31     # dito here GREP_COLOR=1;32  # green
 ##############################################
 # set host variables  
 ##############################################
-#echo KA KB
 [ "$printloadstat" = "true" ] && \
     echo " +onmac $onmac"   && echo " +oncmmc $oncmmc" && \
     echo " +oncmmd $oncmmd" && echo " +oncmpc $oncmpc"
