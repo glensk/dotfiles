1c0353b14b811544094a819e9305a7cb119b4238
glensk <albert.glensk@gmail.com>
2018-10-31 15:07:57 +0100

    Wed Oct 31 15:07:57 CET 2018

---
+++ bin/conda_install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+[ ! -e "$HOME/Downloads" ] && echo not folder $HOME/Downloads && exit
+cd $HOME/Downloads
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+
+
+source ~/.zshrc
+conda activate
+conda install colorama
+conda install -c conda-forge pyfftw=0.10.4
+
--- bin/conda_installed.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-conda install -c conda-forge pyfftw=0.10.4
-conda install -c conda-forge lammps                 # lammps for python
-conda install -c conda-forge phonopy                # phonopy  (if manyally installed needs to be put into PYTHONPATH manually)
