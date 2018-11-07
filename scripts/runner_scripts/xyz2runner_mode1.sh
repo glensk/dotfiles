d3e6c6b7a3d35789572069d40d366ac594c84cb5
glensk <albert.glensk@gmail.com>
2018-11-07 17:08:34 +0100

    Wed Nov  7 17:08:34 CET 2018

--- scripts/runner_scripts/xyz2runner_mode1.sh
+++ scripts/runner_scripts/xyz2runner_mode1.sh
@@ -2,12 +2,15 @@
 
 # for instance in /scratch/glensk/raw_data/test_3_at_1000K_for_fps
 
-filename="input.data.all"
-rm -f $filename  
-touch $filename 
-files=`find . -maxdepth 4 -name simulation.pos_0.xyz`
-for i in $files;do
-    xyz2runner.sh $i >> $filename
-done
+# filename="input.data.all"
+# rm -f $filename  
+# touch $filename 
+# files=`find . -maxdepth 4 -name simulation.pos_0.xyz`
+# for i in $files;do
+#     xyz2runner.sh $i >> $filename
+# done
 
-mkdir 
+rm -f symfun.output
+symfun_gen.py -e Al,Si,Mg -c 16 -n 30 > symfun.output
+
+cp $scripts/runner_scripts/input.nn .
