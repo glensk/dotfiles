354c7f1237f6e65c03fc5e3cd77fc4a6861cae63
glensk <albert.glensk@gmail.com>
2018-11-07 14:29:57 +0100

    Wed Nov  7 14:29:57 CET 2018

--- generalrc/generalrc_alias_bash.sh
+++ generalrc/generalrc_alias_bash.sh
@@ -2,7 +2,8 @@
 
 # everything works perfect even mcd k/k/k
 # for bash transferability ls; needs the ; at the end
-function cd { builtin cd "$@" ; settitlepath.sh; ls; };
+#function cd { builtin cd "$@" ; settitlepath.sh; ls; };
+#function cd { builtin cd "$@" ; ls; };  # even this seems to make probs with aiida verdi computer test daint
 function mcd () { mkdir -p $1;cd $1; }
 alias -- -='cd -' #alias \\-='cd -' 
 function .. { cd ..; }
