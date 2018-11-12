8ed9438e539c3dec20811e99ca30f746cc8177f0
glensk <albert.glensk@gmail.com>
2018-11-12 11:38:02 +0100

    Mon Nov 12 11:38:02 CET 2018

--- scripts/ipi-kmc/kmc_tarjob.sh
+++ scripts/ipi-kmc/kmc_tarjob.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
+hier=`pwd`
 
+cd $hier
 files=`find . -name KMC_QCACHE`
 for i in $files;do
-    echo $i
+    folder=`echo $i | sed 's|/KMC_QCACHE$||g'`
+    cd $hier
+    cd $folder
+    pwd
+    du -sh KMC_QCACHE
+    tar.gz_file_or_folder_remove_origfiles.sh KMC_QCACHE
+    cd $hier
+done
+
+cd $hier
+files=`find . -name log.i-pi`
+for i in $files;do
+    folder=`echo $i | sed 's|/log.i-pi$||g'`
+    cd $hier
+    cd $folder
+    pwd
+    tar.gz_file_or_folder_remove_origfiles.sh log*
+    cd $hier
 done
 
