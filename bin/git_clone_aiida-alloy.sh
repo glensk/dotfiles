9e107161b634a9936c1d756b8a12dde995b6a0b1
glensk <albert.glensk@gmail.com>
2018-11-21 14:50:26 +0100

    Wed Nov 21 14:50:26 CET 2018

---
+++ bin/git_clone_aiida-alloy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+git clone https://gitlab.com/daniel.marchand/aiida-alloy.git
--- bin/git_gitup_gitdown_without_password.sh
+++ bin/git_gitup_gitdown_without_password.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+if [ ! -e ".git" ];then
 echo dotfiles $dotfiles
 cd $dotfiles
+else
+echo current folder `pwd`
+fi
 git config credential.helper store
 git push `git config --get remote.origin.url`
 
