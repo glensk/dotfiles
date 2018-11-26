5d348d2faa2e2758706a37334e2c6e6509f0682c
glensk <albert.glensk@gmail.com>
2018-11-26 16:06:02 +0100

    Mon Nov 26 16:06:02 CET 2018

--- scripts/runner_scripts/n2p2_install.sh
+++ scripts/runner_scripts/n2p2_install.sh
@@ -8,7 +8,9 @@ cd $HOME/Dropbox/Albert/git
 cd n2p2/src
 #make libnnpif-static
 #make libnnptrain-static
-make static
+make static  
+# in my case /usr/include/eigen3/Eigen/src/Core/util/MKL_support.h has in line 56 a preprocessor command #if defined EIGEN_USE_MKL
+# the EIGEN_USE_MKL is actually not acitvaed in the makefiel but is somehow, during the compilation set (which makes the compilation crash)
 
 #####################################################################
 # now the part to make lammps work with n2p2
