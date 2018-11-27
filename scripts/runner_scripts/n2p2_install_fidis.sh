d9253659d5c4c2c4851a0699deb6752a06d75200
glensk <albert.glensk@gmail.com>
2018-11-27 09:20:12 +0100

    Tue Nov 27 09:20:11 CET 2018

--- scripts/runner_scripts/n2p2_install_fidis.sh
+++ scripts/runner_scripts/n2p2_install_fidis.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+installfolder=$HOME  # folder where n2p2 will be installed
 # on fidis do: intel/18.0.2   2) intel-mpi/2018.2.199   3) gsl/2.4   4) eigen/3.3.4
 module load intel
 module load intel-mpi
@@ -11,19 +12,38 @@ module load eigen
 #####################################################################
 # get n2p2 
 #####################################################################
-cd $HOME/Dropbox/Albert/git
+cd $installfolder
+#[ "$USER" = [ "glensk" ] && cd $HOME/Dropbox/Albert/git
 [ ! -e "n2p2" ] && git clone https://github.com/CompPhysVienna/n2p2.git
 cd n2p2/src
-#make libnnpif-static
-#make libnnptrain-static
-make static  
+
+# adapt makefile
+sed -i 's|^COMP=.*|COMP=intel|' makefile
+sed -i 's|^PROJECT_DIR.*|PROJECT_DIR=./|' makefile
+sed -i 's|^LIB=.*|libnnp.so libnnpif.so libnnptrain.so|' makefile # remove pynnp.so
+
+
+# adapt makefile.intel
+sed -i 's|^PROJECT_GSL=.*|PROJECT_GSL=${GSL_ROOT}/include|' makefile.intel
+sed -i 's|^PROJECT_EIGEN=.*|PROJECT_EIGEN=${EIGEN_ROOT}/include/eigen3|' makefile.intel
+line_eigen=`grep -n PROJECT_EIGEN makefile.intel | sed 's|:.*||'`
+sed -i ''"$line_eigen"'i\MKL_INCLUDE=${MKLROOT}/include' makefile.intel
+sed -i 's|^PROJECT_LDFLAGS_BLAS=.*|PROJECT_LDFLAGS_BLAS=-L${GSL_ROOT}/lib -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl|' makefile.intel
+
+# adapt libnnptrain/makefile
+sed -i 's|^INCLUDES=.*|INCLUDES=-I./ -I${PROJECT_INCLUDE}/ -I${PROJECT_GSL} -I${PROJECT_EIGEN} -I${MKL_INCLUDE}|' libnnptrain/makefile
+
+# make
+make
+
+# on cosmopc:
 # in my case /usr/include/eigen3/Eigen/src/Core/util/MKL_support.h has in line 56 a preprocessor command #if defined EIGEN_USE_MKL
 # the EIGEN_USE_MKL is actually not acitvaed in the makefiel but is somehow, during the compilation set (which makes the compilation crash)
 
 #####################################################################
 # now the part to make lammps work with n2p2
 #####################################################################
-
+exit
 # from https://compphysvienna.github.io/n2p2/a01203.html
 pwd
 make libnnpif-shared
