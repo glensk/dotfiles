7b6028cdccf667af18fdaacb8017f4789223b1ec
glensk <albert.glensk@gmail.com>
2018-11-28 17:59:59 +0100

    Wed Nov 28 17:59:59 CET 2018

--- scripts/runner_scripts/n2p2_install_fidis.sh
+++ scripts/runner_scripts/n2p2_install_fidis.sh
@@ -2,14 +2,13 @@
 
 #installfolder=$HOME  # folder where n2p2 will be installed
 # on fidis do: intel/18.0.2   2) intel-mpi/2018.2.199   3) gsl/2.4   4) eigen/3.3.4
-eval module load intel
-exit
+
+module load intel
 module load intel-mpi
 module load intel-mkl
 module load gsl
 module load eigen
 # gives intel/18.0.2   2) intel-mpi/2018.2.199   3) gsl/2.4   4) eigen/3.3.4
-exit
 #####################################################################
 # get n2p2 
 #####################################################################
