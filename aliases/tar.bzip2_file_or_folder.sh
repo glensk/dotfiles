2ef45ea9e3384702544b683eb3b966edac664e43
glensk <albert.glensk@gmail.com>
2019-01-22 10:03:30 +0100

    Tue Jan 22 10:03:26 CET 2019

--- aliases/tar.bzip2_file_or_folder.sh
+++ aliases/tar.bzip2_file_or_folder.sh
@@ -3,3 +3,8 @@
 [ -e "$1.tar.bzip2" ] && echo $1.tar.bzip2 does already exist && exit
 tar --use-compress-program=lbzip2 -cvf "$1.tar.bzip2" $1
 
+###########################################################
+# if lbzip2 not available
+###########################################################
+#cd $dotfiles/sources
+
