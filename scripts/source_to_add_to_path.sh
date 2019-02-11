d19ce6c0b83a9ec98328fd1cd7761688691b2af7
glensk <albert.glensk@gmail.com>
2019-02-11 11:40:42 +0100

    Mon Feb 11 11:40:41 CET 2019

--- scripts/source_to_add_to_path.sh
+++ scripts/source_to_add_to_path.sh
@@ -10,21 +10,21 @@ else
 fi
 
 # define the folders which are given in this git repo 
-ipi="$SCR/i-pi-mc_scripts"
-runner="$SCR/runner_scripts"
-n2p2="$SCR/n2p2"
-aiida_s="$SCR/qe-aiida/aiida_solutejobs_scripts"
-aiida_a="$SCR/qe-aiida/aiida_analyze"
-aiida_b="$SCR/qe-aiida/aiida_submitskripts"
-lammps1="$SCR/lammps_executables"
-lammps2="$SCR/lammps_scripts"
-ase_lammps="$HOME/sources/lammps_source_cosmo"
-n2p2_lib="$HOME/Dropbox/Albert/git/n2p2/lib"
+_ipi="$SCR/i-pi-mc_scripts"
+_runner="$SCR/runner_scripts"
+_n2p2="$SCR/n2p2"
+_aiida_s="$SCR/qe-aiida/aiida_solutejobs_scripts"
+_aiida_a="$SCR/qe-aiida/aiida_analyze"
+_aiida_b="$SCR/qe-aiida/aiida_submitskripts"
+_lammps1="$SCR/lammps_executables"
+_lammps2="$SCR/lammps_scripts"
+_ase_lammps="$HOME/sources/lammps_source_cosmo"
+_n2p2_lib="$HOME/Dropbox/Albert/git/n2p2/lib"
 
-addeverywhere="$ipi:$n2p2:$runner:$aiida_s:$aiida_a:$aiida_b:$lammps1:$lammps2:$ase_lammps/python"
+addeverywhere="$_ipi:$_n2p2:$_runner:$_aiida_s:$_aiida_a:$_aiida_b:$_lammps1:$_lammps2:$_ase_lammps/python"
 
 export PATH="$PATH:$addeverywhere"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$ase_lammps/src:$n2p2_lib"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$ase_lammps/src:$_n2p2_lib"
 
 export LAMMPS_COMMAND="$SCR/executables/lmp_$onhost"
 export IPI_COMMAND="$HOME/Dropbox/Albert/git/i-pi-mc/bin/i-pi"
