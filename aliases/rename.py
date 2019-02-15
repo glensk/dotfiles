107aa29f9ec5a630bef19d43cfdfa18b6a2fe585
glensk <albert.glensk@gmail.com>
2019-02-15 18:01:58 +0100

    Fri Feb 15 18:01:58 CET 2019

--- aliases/rename.py
+++ aliases/rename.py
@@ -9,10 +9,13 @@ def rename(args):
         #new_name = re.sub(args.replace, r'\1_\2\3', i)
         #print(i,"to",new_name)
         #l = re.compile("(?<!^)\s+(?=[A-Z])(?!.\s)").split(s)
-        l = re.compile(args.replace).split(i)
-        for idxj,j in enumerate(l):
-            if j == "":
-                l[idxj] = args.withexp
+        if args.withexp != False:
+            l = re.compile(args.replace).split(i)
+            for idxj,j in enumerate(l):
+                if j == "":
+                    l[idxj] = args.withexp
+        elif args.append != False:
+            l = i+args.append
         #print(i,"to",i.split(args.replace))
         newname = ''.join(l)
         print(i,"to",newname)
@@ -25,7 +28,8 @@ if __name__ == '__main__':
         string = '''
         e.g. rename.py -f simulation.pos_* -r .extxyz$ -w .xyz     # shows all changes (.extexy to xyz)
         e.g. rename.py -f simulation.pos_* -r .extxyz$ -w .xyz -d  # ranemes all .extexy to xyz
-        e.g. rename.py -f cosmo* -r .$ -w .md   # justa append .md to every file
+        e.g. rename.py -f * -a .md # justa append .md to every file
+
 
 
         Nothing will every happen until you specity the option: -d / --doit
@@ -33,11 +37,12 @@ if __name__ == '__main__':
         p = argparse.ArgumentParser(description=string,
                 formatter_class=argparse.RawTextHelpFormatter)
                 #ArgumentDefaultsHelpFormatter)
-        p.add_argument('-f',   '--files', required=True, help='regular expression used to search files', type=str, nargs='+')
-        p.add_argument('-r','--replace',  required=True, help='regular expression to be replaced e.g. \".extxyz$\"', type=str)
-        p.add_argument('-w','--withexp',  required=True, help='replace with this expression/string e.g. \"xyz\"', type=str)
-        p.add_argument('-d','--doit',     required=False, help="If you really want to renae the files/folders, specify this option",
-                default=False,action='store_true')
+        p.add_argument('-f',   '--files', required=True,  help='regular expression used to search files', type=str, nargs='+')
+        p.add_argument('-r','--replace',  required=False, default=False,help='regular expression to be replaced e.g. \".extxyz$\"', type=str)
+        p.add_argument('-a','--append',   required=False, default=False,help='append \"string\" to filenema', type=str)
+        p.add_argument('-w','--withexp',  required=False, default=False,help='replace with this expression/string e.g. \"xyz\"', type=str)
+        p.add_argument('-d','--doit',     required=False, default=False,help="If you really want to renae the files/folders, specify this option",
+                action='store_true')
         p.add_argument('-v','--verbose',help='verbose', action='count', default=False)
         return p
     p = help()
