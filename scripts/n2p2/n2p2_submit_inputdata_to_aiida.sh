0047f41e5ab5dbdbc78ac7cc7c9e81ca1bb369a6
glensk <albert.glensk@gmail.com>
2019-02-18 17:29:57 +0100

    Mon Feb 18 17:29:57 CET 2019

--- scripts/n2p2/n2p2_submit_inputdata_to_aiida.sh
+++ scripts/n2p2/n2p2_submit_inputdata_to_aiida.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 [ "$1" = "" ] && echo 'please provide $1 to be the inputxxx.data file to be calculated by DFT/aiida' && exit
 
+### make README
+readmedate=`date +"%Y-%m-%d_%H:%M:%S"`
+filename="README_$readmedate.txt"
+touch $filename
+echo n2p2_submit_inputdata_to_aiida.sh $1 >> $filename
+echo >> $filename
+
+### scp
 bn=`basename $1`
 scp $1 cosmopc:/local/scratch/glensk/Dropbox/Albert/scripts/dotfiles/aiida-alloy
 echo
 echo "now ssh: loading structures as aiida group"
 echo
+
 ssh glensk@cosmopc15.epfl.ch "source /home/glensk/aiida/bin/activate;cd /local/scratch/glensk/Dropbox/Albert/scripts/dotfiles/aiida-alloy; ./load_runner_dataset.py -d $bn -gn $bn.group && verdi group list -A"
 
 echo
@@ -23,6 +32,7 @@ ssh glensk@cosmopc15.epfl.ch "source /home/glensk/aiida/bin/activate;cd /local/s
     --max_active_calculations 300 \
     --sleep_interval 600"
 
+echo retrieve: $bn.group_calc >> $filename
 echo
 echo "now ssh: checking job status"
 echo
