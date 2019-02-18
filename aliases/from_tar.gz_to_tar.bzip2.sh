12e735c5ca88f0bb30e8ab38fb6a6daf1fdf3637
glensk <albert.glensk@gmail.com>
2019-02-18 12:06:11 +0100

    Mon Feb 18 12:06:11 CET 2019

--- aliases/from_tar.gz_to_tar.bzip2.sh
+++ aliases/from_tar.gz_to_tar.bzip2.sh
@@ -3,20 +3,33 @@ tmp=$HOME/tmp_tar
 [ ! -e "$tmp" ] && mkdir $tmp
 hier=`pwd`
 files=`find $hier -name "*.tar.gz"`
+#files=`find $hier -name "OUTCAR.gz"`
+#files=`find $hier -name "vasprun.xml.gz"`
+for i in $files;do
+    echo $i
+done
+#exit
+outof=`echo "$files" | wc -l`
+
+count=1
 for i in $files;do
     cd $hier
     [ "`ls $tmp`" != "" ] && echo "$tmp is not empty (start)" && exit
     echo
     filename=`basename $i`
     BASEDIR=$(dirname "$i")
-    echo "1 $i"
-    echo "2 $filename"
-    echo "3 $BASEDIR"
+    echo "COUONT $count out of $outof"
+    count=`echo $count | awk '{print $1+1}'`
+    echo "1 i       : $i"
+    echo "2 filename: $filename"
+    echo "3 BASEDIR : $BASEDIR"
     mv $i $tmp
     cd $tmp
-    echo "4 `ls`"
+    echo "4 ls      : `ls`"
+    [ "`ls`" == "" ] && echo empty 1 && exit
+    [ "`ls $filename`" != "$filename" ] && echo filename 1 && exit
     tar -xvf $filename && rm $filename
-    echo "5 `ls`"
+    echo "5         : `ls`"
     [ -e "$filename" ] && echo "$filename still exist" && exit
     [ "`ls *.tar.gz`" != "" ] && echo "*.tar.gz files exist" && exit
     tar.bzip2_file_or_folder_and_remove_originals.sh *
