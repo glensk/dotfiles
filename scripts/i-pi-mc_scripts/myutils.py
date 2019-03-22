e4fc55ee2af33c917935037a7ca53ac7a6040179
glensk <albert.glensk@gmail.com>
2019-03-22 17:32:40 +0100

    Fri Mar 22 17:32:39 CET 2019

--- scripts/i-pi-mc_scripts/myutils.py
+++ scripts/i-pi-mc_scripts/myutils.py
@@ -10,8 +10,11 @@ from shutil import copyfile
 from subprocess import check_output,call
 from datetime import datetime as datetime   # datetime.datetime.now()
 from ase.build import bulk as ase_build_bulk
-from ase.constraints import StrainFilter,ExpCellFilter
-from numba import njit
+from ase.constraints import StrainFilter
+try:  # not in aiida ase
+    from ase.constraints import ExpCellFilter
+except ImportError:
+    pass
 try:
     from ase.calculators.lammpslib import LAMMPSlib
 except ImportError:
@@ -23,7 +26,10 @@ from ase.io import write as ase_write
 from ase.optimize import BFGS
 from ase.optimize import LBFGS
 from ase.optimize import FIRE
-from ase.optimize import GPMin
+try:
+    from ase.optimize import GPMin
+except ImportError:
+    pass
 from ase.optimize.basin import BasinHopping
 from ase.optimize.minimahopping import MinimaHopping
 import shutil
@@ -31,18 +37,6 @@ import time
 
 
 start_time = time.time()
-
-@njit
-def multidet(a,b,c):
-    n=a.shape[0]
-    d=np.empty(n)
-    for i in range(n):
-        u,v,w=a[i],b[i],c[i]
-        d[i]=\
-        u[0]*(v[1]*w[2]-v[2]*w[1])+\
-        u[1]*(v[2]*w[0]-v[0]*w[2])+\
-        u[2]*(v[0]*w[1]-v[1]*w[0])  # 14 operations / det
-    return d
 
 def grep(filepath,string):
     out = []
