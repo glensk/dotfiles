0f23918470a5a4a09e6ca8d26b528985a82231fd
glensk <albert.glensk@gmail.com>
2019-06-12 16:51:57 +0200

    Wed Jun 12 16:51:57 CEST 2019

--- scripts/i-pi-mc_scripts/getEnergies_byLammps.py
+++ scripts/i-pi-mc_scripts/getEnergies_byLammps.py
@@ -1626,10 +1626,10 @@ def get_elastic_constants_al_ext(ace):
         frame_al = my.get_ase_atoms_object_kmc_al_si_mg_vac(ncell=1,nsi=0,nmg=0,nvac=0,a0=4.045,cubic=True,create_fake_vacancy=False,whichcell="fcc")
         ace.get_elastic_external(atomsin=frame_al,verbose=ace.verbose,text="Al_fcc bulk 4at",get_all_constants=True)
         print('ace.c44:',ace.c44,type(ace.c44))
-        filename = ace.pot.potpath+"/elastic_"+str(ace.pot.potepoch_bestteste)+".dat"
-        if not os.path.isfile(filename):
-            np.savetxt(filename,np.array([np.float(ace.c44)]))
-            my.create_READMEtxt(os.getcwd())
+        #filename = ace.pot.potpath+"/elastic_"+str(ace.pot.potepoch_bestteste)+".dat"
+        #if not os.path.isfile(filename):
+        #    np.savetxt(filename,np.array([np.float(ace.c44)]))
+        #    my.create_READMEtxt(os.getcwd())
     return ace.c44
 
 def get_elastic_constants_al_from_ene(ace):
