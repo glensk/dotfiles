4e37ddf70664a467feaf2d4f4373d80b567ba7c0
glensk <albert.glensk@gmail.com>
2019-06-14 16:38:08 +0200

    Fri Jun 14 16:38:07 CEST 2019

--- scripts/potentials/get_origsubmit.sh
+++ scripts/potentials/get_origsubmit.sh
@@ -5,10 +5,15 @@ for f in $ff;do
     echo
     cd $hier
     cd $f
-    ll=`ls README_* | wc -l`
-    pwd_=`cat README_* | grep "# pwd:"`
+    [ -e "submit_n2p2_train.sh" ] && continue #echo "-->" $f exists && continue
+    #ll=`ls README_* | wc -l`
+    ll=`find . -name "README_*" | wc -l`
+    [ "$ll" == "0" ] && continue
+    pwd_=`cat README_* | grep "# pwd:" | awk '{print $3}' | sed 's|/potential$||'`
+    [ -e "$pwd_/submit_training.sh" ] && cp $pwd_/submit_training.sh submit_n2p2_train.sh
+    [ -e "$pwd_/submit_n2p2_train.sh" ] && cp $pwd_/submit_n2p2_train.sh submit_n2p2_train.sh
     
-    echo $f $ll $pwd_
+    echo "-->" $f :$ll:$ll: $pwd_
     cd $hier
 done
 
