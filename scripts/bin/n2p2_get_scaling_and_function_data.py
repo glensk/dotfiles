3e2758f548be6a1568159266493fd4e69ee04976
glensk <albert.glensk@gmail.com>
2019-06-14 17:08:58 +0200

    Fri Jun 14 17:08:58 CEST 2019

--- scripts/bin/n2p2_get_scaling_and_function_data.py
+++ scripts/bin/n2p2_get_scaling_and_function_data.py
@@ -11,11 +11,7 @@ def help(p = None):
             formatter_class=argparse.RawTextHelpFormatter)
     p.add_argument('-c', '--cores' , required=False, help='cores to use for the job', type=int, default=28)
     p.add_argument('-v','--verbose', help='verbose', action='count', default=False)
-<<<<<<< HEAD
     p.add_argument('-n','--normalque', help='use normal que instead of debug que', action='store_true', default=False)
-=======
-    p.add_argument('-d','--debug', help='use debug que', action='count', default=True)
->>>>>>> 1d634a66223c61d8fe8a4fbee536b2246c2fe485
     return p
 
 def todo(args):
@@ -24,13 +20,9 @@ def todo(args):
 if __name__ == '__main__':
     p = help()
     args = p.parse_args()
-<<<<<<< HEAD
     my.print_args(args)
     print('debug?',not args.normalque)
     my.n2p2_get_scaling_and_function_data(cores=args.cores,debug=(not args.normalque))
-=======
-    my.n2p2_get_scaling_and_function_data(cores=args.cores,debug=args.debug)
->>>>>>> 1d634a66223c61d8fe8a4fbee536b2246c2fe485
     my.create_READMEtxt(os.getcwd())
 
 
