3aed1791b5a53246e74f10e7ef4a81440337e31d
glensk <albert.glensk@gmail.com>
2020-03-11 15:20:28 +0100

    Wed Mar 11 15:20:28 CET 2020

--- scripts/potentials/get_theta_thetaPrime_phase_transition.py
+++ scripts/potentials/get_theta_thetaPrime_phase_transition.py
@@ -3,6 +3,7 @@
 from __future__ import print_function
 import numpy as np
 import os,sys,argparse,glob
+import matplotlib.pyplot as plt
 
 def help(p = None):
     string = ''' helptext '''
@@ -17,8 +18,18 @@ def todo(args):
     #    sys.exit("inputfile "+args.inputfile+" does not exist")
     folder = glob.glob("n2p2_alcu_v2dm_*")
     tall=[]
-    for f in folder:
-        #print(f)
+    Ball=[]
+    bdall=[]
+    alle = np.zeros((40,8))
+    for idx,f in enumerate(folder):
+        print(f)
+        nr = int(f.split("n2p2_alcu_v2dm_")[1])
+        evt = np.loadtxt(f+"/Theta/evinet/Evinet_1")
+        Bt = evt[2]
+        Bdt = evt[3]
+        evtp = np.loadtxt(f+"/ThetaPrime/evinet/Evinet_1")
+        Btp = evtp[2]
+        Bdtp = evtp[3]
         t = np.loadtxt(f+"/Theta/fah/Fah_surfacefit_upto1000/thermo_3rd/output_0.0001GPa/Gibbs_energy")
         tp = np.loadtxt(f+"/ThetaPrime/fah/Fah_surfacefit_upto1000/thermo_3rd/output_0.0001GPa/Gibbs_energy")
         #print(f,len(t),len(tp))
@@ -31,27 +42,34 @@ def todo(args):
         #sys.exit()
         a=t[:,1]-tp[:,1]
         T=np.where(a<0)[0][0]
-        print(f,'T',T)
+        print(f,'T',T-595.7,'dB',Bt-Btp,'dB',Bdt-Bdtp)
         tall.append(T)
+        alle[nr-1][0] = nr
+        alle[nr-1][0+1] = T
+        alle[nr-1][1+1] = Bt
+        alle[nr-1][2+1] = Btp
+        alle[nr-1][3+1] = Btp-Bt
+        alle[nr-1][4+1] = Bdt
+        alle[nr-1][5+1] = Bdtp
+        alle[nr-1][6+1] = Bdtp-Bdt
     tall = np.array(tall)
     print('mean',tall.mean())
     print('std',tall.std())
 
-    return
+    return alle
 
 if __name__ == '__main__':
     p = help()
     args = p.parse_args()
-    todo(args)
-    #folder = glob.glob("n2p2_alcu_v2dm_*")
-    #for f in folder:
-    #    print(f)
-    #    t = np.loadtxt(f+"/Theta/fah/Fah_surfacefit_upto1000/thermo_3rd/output_0.0001GPa/Gibbs_energy")
-    #    tp = np.loadtxt(f+"/ThetaPrime/fah/Fah_surfacefit_upto1000/thermo_3rd/output_0.0001GPa/Gibbs_energy")
-    #    #t[:,1] = np.array(t-tp)
-    #    print(t)
-    #    print()
-    #    print(t[:,1]-tp[:,1])
-    #    sys.exit()
+    alle= todo(args)
+    plt.plot(alle[:,0],alle[:,1])
 
+    plt.plot([0,40],[alle[:,1].mean(),alle[:,1].mean()],'--',label='average transition temperature (K)')
+    plt.legend()
+    plt.xlabel('Neural Network nr')
+    plt.ylabel('Phase Transition (K)')
+
+    # see no correlation between transition temperateru and B or Ber or differences of those.
+    #import pandas as pd
+    #df = pd.DataFrame(data=alle,  columns=["T", "Bt","Btp","Bdt","Bdtp",'Bdiff','Bddiff'])
 
