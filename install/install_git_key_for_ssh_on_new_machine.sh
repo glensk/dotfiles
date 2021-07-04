ce9d582bc7834bbdc0695df3d033d37fc82fd017
glensk <albert.glensk@gmail.com>
2021-07-04 18:55:39 +0200

    Sun Jul  4 18:55:38 CEST 2021

---
+++ install/install_git_key_for_ssh_on_new_machine.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo
+echo in the next field use a passphrase
+echo
+
+ssh-keygen -t ed25519 -C "albert.glensk@gmail.com"
+echo 
+echo from https://docs.github.com/en/github/authenticating-to-github/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
+echo
+echo "Note: The -K option is Apple's standard version of ssh-add, which stores the passphrase in your keychain for you when you add an ssh key to the ssh-agent. If you chose not to add a passphrase to your key, run the command without the -K option. If you don't have Apple's standard version installed, you may receive an error. For more information on resolving this error, see \"Error: ssh-add: illegal option -- K.\""
+ssh-add -K ~/.ssh/id_ed25519
+
+
+
